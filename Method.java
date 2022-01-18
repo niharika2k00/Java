58ed5c7b977b5cd2523b0e685130b39b4001e941
niharika2k00 <dniharika16@gmail.com>
2022-01-18 16:06:08 +0530

    java prog

--- Method.java
+++ Method.java
@@ -16,9 +16,16 @@ public class Method {
         objA.room = 8;
         objA.color = "Crimson";
         objA.design();
-    }
 
+        System.out.println("\n\n---------------------------------------------------------");
+        System.out.println("---------------------------------------------------------");
+        Student s1 = new Student("John Doe", 85426, "Coder");
+        Student s2 = new Student("Genelia", 12537, "Dancer");
+        s1.createStudent();
+        s2.createStudent();
+    }
 }
+
 
 class Bunglow {
 
@@ -28,7 +35,7 @@ class Bunglow {
     int room;
     String color;
 
-    // Static Method
+    // Static Method - always contains static variables
     public static void Country(String name) {
         place = name;
     }
@@ -39,6 +46,25 @@ class Bunglow {
 
     public void design() {
         System.out.println("\n It contains " + balcony + " Balcony And " + room + " Rooms  of " + color + " color. ");
+    }
+
+}
+
+
+class Student {
+    String name;
+    int roll;
+    String passion;
+
+    // Constructor
+    Student(String name, int roll, String passion) {
+        this.name = name;
+        this.roll = roll;
+        this.passion = passion;
+    }
+
+    public void createStudent() {
+        System.out.println("New Student created :  \n\t" + name + "\t" + roll + "\t" + passion);
     }
 
 }
--- Second_Largest_Ele.java
+++ Second_Largest_Ele.java
@@ -1,28 +1,26 @@
 
-public class FindSecondLargestEle {
+public class Second_Largest_Ele {
 
-    public static void findSecondLarge(int[] arr, int len) {
+    // Time Complexity : O(n)
+    public static int findSecondLarge(int[] arr, int len) {
         System.out.println(len);
         int i, large = 0, secondLarge = -1; //  stores index
         for (i = 1; i < len; i++) {
             if (arr[i] > arr[large]) {
                 secondLarge = large;
-                large = arr[i];
+                large = i;
             }
             else {
                 if (secondLarge == -1 || arr[secondLarge] < arr[i])
                     secondLarge = i;
             }
         }
-        // return secondLarge;
-        System.out.println(secondLarge);
+        return secondLarge;
     }
 
     public static void main(String[] args) {
         int arr[] = { 10, 66, 4, 22 };
-        int len = arr.length;
-        // int ans = findSecondLarge(arr, len);
-        findSecondLarge(arr, len);
-        System.out.println("Second Largest Element in the array : " + len);
+        int len = arr.length, ans = findSecondLarge(arr, len);
+        System.out.println("Second Largest Element in the array : " + arr[ans]);
     }
 }
