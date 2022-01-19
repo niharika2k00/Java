a278f04700fa6e71d072c89ecd820cb424bd90a4
niharika2k00 <dniharika16@gmail.com>
2022-01-20 02:43:45 +0530

    collection framework..

--- Array_List.java
+++ Array_List.java
@@ -3,15 +3,23 @@ import java.io.*;
 import java.util.*;
 import java.util.ArrayList;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class Array_List {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         int n = 5, i;
-        ArrayList<Integer> arrList_A = new ArrayList<Integer>(n);
-        ArrayList<Integer> vec = new ArrayList<Integer>(4);
         Manupulation Obj = new Manupulation();
         Scanner sc = new Scanner(System.in);
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // ---------------------------------------------------------------------
+        //                        ArrayList
+        // ---------------------------------------------------------------------
+        ArrayList<Integer> arrList_A = new ArrayList<Integer>(n);
 
         for (i = 0; i < n; i++) // Append
             arrList_A.add(i);
@@ -22,13 +30,30 @@ public class Array_List {
 
         Obj.printElement(arrList_A);
 
+        // ---------------------------------------------------------------------
+        //                       User Input uisng ArrayList
+        // ---------------------------------------------------------------------
+        ArrayList<Integer> vec = new ArrayList<Integer>(4);
         System.out.println("\n\nEnter elements for ArrayList : ");
         for (int j = 0; j <= 3; j++) {
-            int ele = sc.nextInt(); // sc.nextInt(); 
+            // int ele = sc.nextInt(); 
+            int ele = Integer.parseInt(br.readLine());
             vec.add(ele);
         }
 
         Obj.printElement(vec);
+
+        // ---------------------------------------------------------------------
+        //                       String of Array using ArrayList
+        // ---------------------------------------------------------------------
+        ArrayList<String> stringVec = new ArrayList<>();
+        stringVec.add("Computer");
+        stringVec.add("Engineering");
+        stringVec.add(1, "Science");
+
+        System.out.println();
+        for (String str : stringVec)
+            System.out.print(str + " ");
     }
 }
 
@@ -42,7 +67,6 @@ class Manupulation {
 
         for (i = 0; i < len; i++)
             System.out.print(arrList.get(i) + " ");
-
         System.out.println();
 
         for (int ele : arrList) {
