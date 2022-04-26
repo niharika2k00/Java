f2339a9355b96f3947a624241a3646f660297132
niharika2k00 <dniharika16@gmail.com>
2022-04-26 23:50:41 +0530

    GFG pointers rev

--- Pointer.c
+++ Pointer.c
@@ -40,20 +40,31 @@ int main()
     // printf("Size of the Pointer  :  %d\n", sizeof(d));
 
     // *************  TYPE - 4   *************
-    float arr[5] = {12.5, 10.0, 13.5, 90.5, 0.5};
-    float *ptr1 = &arr[0];
-    float *ptr2 = ptr1 + 3; // ptr2 points to the  [3 * sizeof(int)] = 3 * 4 = 12 means points 90.5
+    // float arr[5] = {12.5, 10.0, 13.5, 90.5, 0.5};
+    // float *ptr1 = &arr[0];
+    // float *ptr2 = ptr1 + 3; // ptr2 points to the  [3 * sizeof(int)] = 3 * 4 = 12 means points 90.5
 
-    printf("%f ", *ptr2);      // Value :  90.500000
-    printf("%d", ptr2 - ptr1); //  [12 - 0] / 4 (size of int)  =  12 / 4 = 3
+    // printf("%f ", *ptr2);      // Value :  90.500000
+    // printf("%d", ptr2 - ptr1); //  [12 - 0] / 4 (size of int)  =  12 / 4 = 3
 
     // *************  TYPE - 5   *************
-    int arr[] = {1, 9, 3, 4, 5};
-    int *p = arr;
+    // int arr[] = {1, 9, 3, 4, 5};
+    // int *p = arr;
 
-    ++*p;   //  ++(*p)  increment value of arr[0].
-    p += 2; //  shift +2  cell points to the 3rd element.
-    printf("%d", *p);
+    // ++*p;   //  ++(*p)  increment value of arr[0].
+    // p += 2; //  shift +2  cell points to the 3rd element.
+    // printf("%d", *p);
+
+    // *************  TYPE - 6  [all pointing the Base Address]  *************
+    int arr[4][4];
+    printf("%d\n", arr);
+    printf("%d\n", *arr);
+    printf("%d\n", arr[0]);
+
+    char g[] = "geeksforgeeks";
+    // printf("%s\n", g[6]);
+
+    printf("%s", g + g[6] - g[8]);
 
     return 0;
 }
